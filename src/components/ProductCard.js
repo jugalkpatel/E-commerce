5335951a4d8801c0e53e17239321cc9111d9fc0a
jugalkpatel <ptjimmy99@gmail.com>
2021-04-19 14:08:47 +0530

    feat: completed product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,43 +1,103 @@
 import React from "react";
 import "./ProductCard.css";
 import closeBtn from "../assets/svgs/close-btn.svg";
-import wishBtn from "../assets/svgs/product-wish.svg";
+import bookmark from "../assets/svgs/bookmark.svg";
 import rating from "../assets/svgs/rating.svg";
+import arrow from "../assets/svgs/right-arrow.svg";
 const ProductCard = () => {
+    const products = [
+        {
+            id: 1,
+            name: "NVIDIA GEFORCE RTX 3060 Ti",
+            imageUrl: "https://assets.nvidia.partners/images/png/nvidia-geforce-rtx-3060-ti.png",
+            specifications: {
+                cooling_system: "fan",
+                clock_speed: 1.67,
+                memory_size: 8
+            },
+            price: 800
+        },
+        {
+            id: 2,
+            name: "NVIDIA GEFORCE RTX 3060 Ti",
+            imageUrl: "https://assets.nvidia.partners/images/png/nvidia-geforce-rtx-3060-ti.png",
+            specifications: {
+                cooling_system: "fan",
+                clock_speed: 1.67,
+                memory_size: 8
+            },
+            price: 800
+        },
+        {
+            id: 3,
+            name: "NVIDIA GEFORCE RTX 3060 Ti",
+            imageUrl: "https://assets.nvidia.partners/images/png/nvidia-geforce-rtx-3060-ti.png",
+            specifications: {
+                cooling_system: "fan",
+                clock_speed: 1.67,
+                memory_size: 8
+            },
+            price: 800
+        },
+        {
+            id: 4,
+            name: "NVIDIA GEFORCE RTX 3060 Ti",
+            imageUrl: "https://assets.nvidia.partners/images/png/nvidia-geforce-rtx-3060-ti.png",
+            specifications: {
+                cooling_system: "fan",
+                clock_speed: 1.67,
+                memory_size: 8
+            },
+            price: 800
+        }
+
+
+    ]
     return (
         <div class="product-container">
             <a href="#" class="product-info">
                 <span class="product__img__container">
+                    <button class="close-btn">
+                        <img src={closeBtn} alt="close_btn" />
+                    </button>
                     <img
                         class="product-img product-info__img"
-                        src="https://source.unsplash.com/random/"
+                        src="https://assets.nvidia.partners/images/png/nvidia-geforce-rtx-3060-ti.png"
                         alt="product"
                     />
                     <button class="wish-btn">
-                        <img src={wishBtn} alt="wish_btn" />
+                        <img src={bookmark} alt="wish_btn" />
                     </button>
                 </span>
                 <span class="product-info__text">
-                    lorem ipsum Lorem ipsum dolor sit amet consectetur
-                    adipisicing elit. Distinctio, veniam!
+                    NVIDIA GEFORCE RTX 3060 Ti
                 </span>
-                <span class="product-info__small">
-                    Lorem ipsum dolor sit Lorem Lorem, ipsum dolor sit amet
-                    consectetur adipisicing elit. Quisquam, ipsa.
-                </span >
-                <span class="product-info__rating">
-                    <img src={rating} alt="rating_icon" />
-                4.5
+                <span class="product-info__specifications">
+                    <ul className="specifications__list">
+                        <li className="specifications__list__item">
+                            <img className="specifications__arrow" src={arrow} alt="right_arrow" />
+                            Cooling System: Fan
+                        </li>
+                        <li className="specifications__list__item">
+                            <img className="specifications__arrow" src={arrow} alt="right_arrow" />
+                            Boost Clock Speed: 1.67 GHz
+                        </li>
+                        <li className="specifications__list__item">
+                            <img className="specifications__arrow" src={arrow} alt="right_arrow" />
+                            GPU Memory Size: 8GB
+                        </li>
+                    </ul>
                 </span>
+
                 <span class="price-tag">Rs. 250</span>
             </a>
             <button class="primary-btn">ADD TO CART</button>
-            <div class="overlay-div">
-                <button class="close-btn">
-                    <img src={closeBtn} alt="close-btn" />
-                </button>
-                <span class="overlay-text"> OUT OF STOCK </span>
-            </div>
+            {/* <div class="overlay-div">
+               <button class="close-btn">
+                   <img src={closeBtn} alt="close-btn" />
+               </button>
+               <span class="overlay-text"> OUT OF STOCK </span>
+           </div>  */}
         </div>
     )
 }
