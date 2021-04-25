cb2cb0be6ca6d75f9b51cf145ba0641dfd161e12
jugalkpatel <ptjimmy99@gmail.com>
2021-04-26 00:37:20 +0530

    style: change header zindex

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,9 @@ import gpu from "../../assets/svgs/gpu.svg";
 import bookmark from "../../assets/svgs/bookmark-nv.svg";
 import profile from "../../assets/svgs/profile.svg";
 import cart from "../../assets/svgs/cart.svg";
+import { useAppData } from "../../contexts/AppDataProvider";
 const Header = () => {
+    const { appData: { cartData, wishListData } } = useAppData();
     return (
         <div className="header">
             <a className="header__logo">
@@ -20,19 +22,24 @@ const Header = () => {
                     <li className="header__navigation__list__item">
                         <a href="#" className="badge-icon">
                             <img src={cart} alt="cart_icon" />
-                            <span className="num-badge cart-badge">10</span>
+                            {
+                                cartData.length > 0 && <span className="num-badge cart-badge">
+                                    {cartData.length}
+                                </span>
+                            }
                         </a>
                     </li>
                     <li className="header__navigation__list__item">
                         <a href="#" className="badge-icon">
                             <img src={bookmark} alt="bookmark__icon" />
-                            <span className="num-badge cart-badge">10</span>
+                            {
+                                wishListData.length > 0 && <span className="num-badge cart-badge">10</span>
+                            }
                         </a>
                     </li>
                     <li className="header__navigation__list__item">
                         <a href="#" className="badge-icon">
                             <img src={profile} alt="profile__icon" />
-                            <span className="num-badge cart-badge">10</span>
                         </a>
                     </li>
                 </ul>
